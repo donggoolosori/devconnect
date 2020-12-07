dd68cebb8774a20f484699bea0f5a32ee4352d47
김동준 <dongjune@gimdongjun-ui-MacBookPro.local>
2020-12-07 16:58:08 +0900

    Setting json web token in user registration

--- routes/api/users.js
+++ routes/api/users.js
@@ -2,6 +2,8 @@ const express = require("express");
 const router = express.Router();
 const gravatar = require("gravatar");
 const bcrypt = require("bcryptjs");
+const jwt = require("jsonwebtoken");
+const config = require("config");
 const { check, validationResult } = require("express-validator");
 
 const User = require("../../models/User");
@@ -60,13 +62,25 @@ router.post(
       await user.save();
 
       // Return jsonwebtoken
-      res.send("User Registered");
+      const payload = {
+        user: {
+          id: user.id, //mongoDB에는 _id로 저장되지만 mongoose는 id로 사용
+        },
+      };
+
+      jwt.sign(
+        payload,
+        config.get("jwtSecret"),
+        { expiresIn: 360000 },
+        (err, token) => {
+          if (err) throw err;
+          res.json({ token });
+        }
+      );
     } catch (error) {
       console.error(error.message);
       res.status(500).send("Server error");
     }
-
-    res.send("User route");
   }
 );
 
