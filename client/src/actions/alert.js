50b215742e3c8f573903804e2d3a17018218a6fc
donggoolosori <kdj6551@naver.com>
2020-12-10 11:53:39 +0900

    feat : Connect alert action with Register.js

--- client/src/actions/alert.js
+++ client/src/actions/alert.js
@@ -1,4 +1,4 @@
-import { SET_ALERT, REMOVE_ALERT } from ".types";
+import { SET_ALERT, REMOVE_ALERT } from "./types";
 import { v4 as uuidv4 } from "uuid";
 
 export const setAlert = (msg, alertType) => (dispatch) => {
