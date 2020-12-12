ce9fc0bd0b7e4a6bf97d755ec3f932b8e3b92815
donggoolosori <kdj6551@naver.com>
2020-12-12 12:01:01 +0900

    feat : Create profile actions

--- client/src/App.js
+++ client/src/App.js
@@ -15,6 +15,7 @@ import { Provider } from "react-redux";
 import store from "./store";
 import { loadUser } from "./actions/auth";
 import setAuthToken from "./utils/setAuthToken";
+import CreateProfile from "./components/profile-form/CreateProfile";
 
 const App = () => {
   useEffect(() => {
@@ -36,6 +37,11 @@ const App = () => {
               <Route exact path="/register" component={Register} />
               <Route exact path="/login" component={Login} />
               <PrivateRoute exact path="/dashboard" component={Dashboard} />
+              <PrivateRoute
+                exact
+                path="/create-profile"
+                component={CreateProfile}
+              />
             </Switch>
           </section>
         </Fragment>
