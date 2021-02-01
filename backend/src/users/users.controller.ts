1a5aa3674a2734298a4f924f318831a322f3d5c0
donggoolosori <kdj6551@naver.com>
2021-02-01 17:57:12 +0900

    Create jwt

--- backend/src/users/users.controller.ts
+++ backend/src/users/users.controller.ts
@@ -1,9 +1,14 @@
-import { Controller, Get } from '@nestjs/common';
+import { Body, Controller, Post, Response } from '@nestjs/common';
+import { CreateUserDto } from 'src/dto/user.dto';
+import { Jwt } from 'src/interfaces/jwt.interface';
+import { UsersService } from './users.service';
 
 @Controller('users')
 export class UsersController {
-  @Get()
-  getHello(): string {
-    return 'hello, users';
+  constructor(private readonly userService: UsersService) {}
+
+  @Post()
+  registerUser(@Body() createUserDto: CreateUserDto): Promise<String> {
+    return this.userService.registerUser(createUserDto);
   }
 }
--- backend/src/users/users.module.ts
+++ backend/src/users/users.module.ts
@@ -1,9 +1,13 @@
 import { Module } from '@nestjs/common';
+import { MongooseModule } from '@nestjs/mongoose';
+import { User, UserSchema } from 'src/schema/user.schema';
 import { UsersController } from './users.controller';
 import { UsersService } from './users.service';
 
 @Module({
-  imports: [],
+  imports: [
+    MongooseModule.forFeature([{ name: User.name, schema: UserSchema }]),
+  ],
   controllers: [UsersController],
   providers: [UsersService],
 })
