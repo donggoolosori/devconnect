6cfc9430dc5a15f48eb52521ec1f9297218ba62b
donggoolosori <kdj6551@naver.com>
2021-02-03 16:29:53 +0900

    Create user dto

---
+++ backend/src/dto/create-user.dto.ts
@@ -0,0 +1,12 @@
+import { IsEmail, IsNotEmpty, MinLength } from 'class-validator';
+
+export class CreateUserDto {
+  @IsNotEmpty()
+  name: string;
+
+  @IsEmail()
+  email: string;
+
+  @MinLength(6)
+  password: string;
+}
