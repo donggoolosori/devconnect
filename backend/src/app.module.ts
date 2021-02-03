f1b86a1569ea03325defabd900b28f90914e55cb
donggoolosori <kdj6551@naver.com>
2021-02-03 16:06:50 +0900

    Connect mongo db

--- backend/src/app.module.ts
+++ backend/src/app.module.ts
@@ -1,6 +1,13 @@
 import { Module } from '@nestjs/common';
+import { ConfigModule } from '@nestjs/config';
+import { MongooseModule } from '@nestjs/mongoose';
+import { UsersModule } from './users/users.module';
 
 @Module({
-  imports: [],
+  imports: [
+    UsersModule,
+    ConfigModule.forRoot(),
+    MongooseModule.forRoot(process.env.MONGODB_URI),
+  ],
 })
 export class AppModule {}
