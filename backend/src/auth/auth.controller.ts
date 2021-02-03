6d4cc0be6e0d53cc5a50c2423f0e67a2a43540db
donggoolosori <kdj6551@naver.com>
2021-02-03 19:53:40 +0900

    Implement login

--- backend/src/auth/auth.controller.ts
+++ backend/src/auth/auth.controller.ts
@@ -1,14 +1,31 @@
-import { Controller, Get, Request, UseGuards } from '@nestjs/common';
+import {
+  Body,
+  Controller,
+  Get,
+  Post,
+  Request,
+  UseGuards,
+  UsePipes,
+  ValidationPipe,
+} from '@nestjs/common';
+import { Jwt } from 'src/users/interfaces/jwt.interface';
 import { AuthService } from './auth.service';
+import { LoginDto } from './dto/login.dto';
 import { JwtAuthGuard } from './jwt-auth.guard';
 
 @Controller('auth')
 export class AuthController {
   constructor(private readonly authService: AuthService) {}
 
-  @UseGuards(JwtAuthGuard)
   @Get()
+  @UseGuards(JwtAuthGuard)
   getUserInfo(@Request() req): Promise<any> {
     return this.authService.getUserInfo(req.user.id);
+  }
+
+  @Post()
+  @UsePipes(ValidationPipe)
+  login(@Body() loginDto: LoginDto): Promise<any> {
+    return this.authService.login(loginDto);
   }
 }
--- backend/src/auth/auth.service.ts
+++ backend/src/auth/auth.service.ts
@@ -1,13 +1,46 @@
-import { Injectable } from '@nestjs/common';
+import { Injectable, UnauthorizedException } from '@nestjs/common';
 import { InjectModel } from '@nestjs/mongoose';
 import { Model } from 'mongoose';
+import { Jwt } from 'src/users/interfaces/jwt.interface';
 import { User, UserDocument } from 'src/users/schema/User.schema';
+import { LoginDto } from './dto/login.dto';
+import * as bcrypt from 'bcrypt';
+import { JwtService } from '@nestjs/jwt';
 
 @Injectable()
 export class AuthService {
-  constructor(@InjectModel(User.name) private userModel: Model<UserDocument>) {}
+  constructor(
+    @InjectModel(User.name) private userModel: Model<UserDocument>,
+    private readonly jwtService: JwtService,
+  ) {}
 
   async getUserInfo(id: string): Promise<any> {
     return await this.userModel.findById(id).select('-password');
+  }
+
+  async login(loginDto: LoginDto): Promise<any> {
+    const { email, password } = loginDto;
+    let user = await this.userModel.findOne({ email });
+
+    // check user existence
+    if (!user) {
+      throw new UnauthorizedException('Invalid Credentials');
+    }
+
+    // check email, password matching
+    const isMatch = await bcrypt.compare(password, user.password);
+    if (!isMatch) {
+      throw new UnauthorizedException('Invalid Credentials');
+    }
+
+    // return jwt
+    const payload = {
+      user: {
+        id: user.id,
+      },
+    };
+    return {
+      token: this.jwtService.sign(payload),
+    };
   }
 }
