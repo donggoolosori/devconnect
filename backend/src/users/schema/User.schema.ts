ec3f7c6a5097e913a457551decc2b18ea077bedb
donggoolosori <kdj6551@naver.com>
2021-02-03 16:19:30 +0900

    Make user schema

---
+++ backend/src/users/schema/User.schema.ts
@@ -0,0 +1,24 @@
+import { Prop, Schema, SchemaFactory } from '@nestjs/mongoose';
+import { Document } from 'mongoose';
+
+export type UserDocument = User & Document;
+
+@Schema()
+export class User {
+  @Prop({ required: true })
+  name: string;
+
+  @Prop({ required: true })
+  email: string;
+
+  @Prop({ required: true })
+  password: string;
+
+  @Prop()
+  avatar: string;
+
+  @Prop({ default: Date.now })
+  date: string;
+}
+
+export const UserSchema = SchemaFactory.createForClass(User);
