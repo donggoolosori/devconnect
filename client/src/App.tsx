8efa84db71de945db56836982cc43998effd66df
donggoolosori <kdj6551@naver.com>
2021-02-08 17:27:02 +0900

    Make home html

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,7 +1,16 @@
 import React from 'react';
+import { Navbar } from './components/layout/Navbar';
+import { Landing } from './components/layout/Landing';
+
+import './App.css';
 
 interface Props {}
 
 export const App: React.FC<Props> = () => {
-  return <div className="App"></div>;
+  return (
+    <div className="App">
+      <Navbar />
+      <Landing />
+    </div>
+  );
 };
