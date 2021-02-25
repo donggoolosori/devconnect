c8729d01fb54d42432e88793fdbe36c6afadf628
donggoolosori <kdj6551@naver.com>
2021-02-25 13:33:18 +0900

    Deploy react app

--- client/src/axios.ts
+++ client/src/axios.ts
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 const instance = axios.create({
-  baseURL: 'http://localhost:3000',
+  baseURL: 'https://devconnector-dj.herokuapp.com',
   headers: {
     'Content-Type': 'application/json',
   },
