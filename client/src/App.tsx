4011973bdc3d7da10f96e049ec1baaf6fb0d179b
donggoolosori <kdj6551@naver.com>
2021-02-25 17:43:37 +0900

    Change browserouter to hashrouter

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,5 +1,5 @@
 import React, { useEffect } from 'react';
-import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
+import { HashRouter as Router, Route, Switch } from 'react-router-dom';
 import { Navbar } from './components/layout/Navbar';
 import { Landing } from './components/layout/Landing';
 import './App.css';
@@ -33,7 +33,7 @@ export const App: React.FC<Props> = () => {
     dispatch(loadUser());
   }, [dispatch]);
   return (
-    <Router basename={process.env.PUBLIC_URL}>
+    <Router>
       <div className="App">
         <Navbar />
         <Route exact path="/" component={Landing} />
